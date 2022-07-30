c782a305b9d0fde9d177b6927637a256332d1f61
jBlazzzy-tech <dashivegas@mail.ru>
2022-07-30 23:13:38 +0900

    Initial commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,49 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.sql.ResultSet;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+    @Autowired
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public User getUserByCar(String model, int series) {
+        User user = null;
+        try (Session session = sessionFactory.openSession()) {
+            String hql = "from User user where user.car.model = :model and user.car.series = :series";
+            Query query = session.createQuery(hql);
+            query.setParameter("model", model).setParameter("series", series);
+            query.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+}
